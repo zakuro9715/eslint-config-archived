d8a11a64c794c769b89592cf3558fc0af1f56abe
Takahiro Yaota <z@kuro.red>
2016-08-10 21:10:01 +0900

    Setup test

---
+++ test.js
@@ -0,0 +1,16 @@
+var eslint = require('eslint')
+var test = require('tape')
+
+test('config is valid', function (t) {
+  var CLIEngine = eslint.CLIEngine
+
+  var cli = new CLIEngine({
+    useEslintrc: false,
+    configFile: 'index.js'
+  })
+
+  var code = 'function f() {}\nf()\n'
+
+  t.ok(cli.executeOnText(code).errorCount === 0)
+  t.end()
+})
